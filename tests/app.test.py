091b874b88c6334aaf5f62c8729824b28ed02f04
Owen Stillman <owenstillman@live.co.uk>
2020-05-28 14:45:27 +0100

    Prepare HTML output

--- tests/app.test.py
+++ tests/app.test.py
@@ -48,16 +48,19 @@ class DatabaseContentFetched(unittest.TestCase):
         self.app = app.test_client()
         self.db = sqlite3.connect('DB/ostillman.db')
         self.cursor = self.db.cursor()
+        self.entries_count = self.cursor.execute('''select COUNT(id) from entry''').fetchone()[0]
 
     def test_records_exists(self):
         assert hasattr(db, 'Records')
 
     def test_all_records_fetchable(self):
         self.Records = db.Records()
-        self.entries_count = self.cursor.execute('''select COUNT(id) from entry''').fetchone()
-        assert len(self.Records.all_records["Entries"]["Data"]) == self.entries_count[0]
+        assert len(self.Records.all_records["Entries"]["Data"]) == self.entries_count
 
-        
+
+    def test_all_records_output(self):
+        rv = self.app.get('/')
+        assert len(re.findall("record", rv.data.decode("utf-8"))) == self.entries_count
 
 
     def tearDown(self):
