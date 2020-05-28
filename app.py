539ca60c4e270401593b69683e439c669ae21f05
Owen Stillman <owenstillman@live.co.uk>
2020-05-28 15:01:29 +0100

    Records outputting

--- app.py
+++ app.py
@@ -1,10 +1,13 @@
 from flask import Flask, render_template, request
+import db
 
 app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return render_template('index.html')
+    Records = db.Records()
+    timeline_entries = Records.all_records['Entries']['Data']
+    return render_template('index.html', timeline_entries=timeline_entries)
 
 
 if __name__ == '__main__':
