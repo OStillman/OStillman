9b5ba9d368f70e4a231a250ecab8f569f8320cf7
Owen Stillman <owenstillman@live.co.uk>
2020-05-28 13:38:24 +0100

    Begin Development

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # OStillman
+
+Redevelopment of my Website (long overdue...)
+
+Aim is to have a fully functioning website running on my Rapsberry Pi4. 
+
+Will be also following TDD princicples as much as possible throughout development
---
+++ tests/app.test.py
@@ -0,0 +1,27 @@
+import unittest
+from os import path
+import sys
+
+sys.path.append( path.dirname( path.dirname( path.abspath(__file__) ) ) )
+
+from app import app
+
+class BasicTestCase(unittest.TestCase):
+
+    def test_index_reachable(self):
+        self.tester = app.test_client(self)
+        self.response = self.tester.get('/', content_type='html/text')
+        self.assertEqual(self.response.status_code, 200)
+
+class FunctionalityTestCase(unittest.TestCase):
+
+    def setUp(self):
+        self.app = app.test_client()
+    
+    def test_index_title(self):
+        rv = self.app.get('/')
+        assert b'Owen Stillman' in rv.data
+
+
+if __name__ == '__main__':
+    unittest.main()
