f08e646462b646295220eddecd85db745716624b
Owen Stillman <owenstillman@live.co.uk>
2020-06-02 11:00:00 +0100

    Add /ghome endpoint

--- app.py
+++ app.py
@@ -12,6 +12,13 @@ def index():
     Name = db.Name()
     return render_template('index.html', timeline_entries=timeline_entries, blurb=Bio.bio, name=Name.name)
 
+@app.route('/ghome', methods=['GET', 'POST'])
+def ghome():
+    if request.method == 'GET':
+        return 'ok'
+    else:
+        return "", 200, {'ContentType':'application/json'} 
+
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0')
